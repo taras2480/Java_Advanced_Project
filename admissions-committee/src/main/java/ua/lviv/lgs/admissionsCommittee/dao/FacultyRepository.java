bd10b2a13c2012ca71f25adada8dd286d82d55a1
taras2480 <aviator2480@gmail.com>
2020-04-06 15:11:57 +0300

    added annotation @Repository

--- admissions-committee/src/main/java/ua/lviv/lgs/admissionsCommittee/dao/FacultyRepository.java
+++ admissions-committee/src/main/java/ua/lviv/lgs/admissionsCommittee/dao/FacultyRepository.java
@@ -1,9 +1,11 @@
 package ua.lviv.lgs.admissionsCommittee.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ua.lviv.lgs.admissionsCommittee.domain.Faculty;
 
+@Repository
 public interface FacultyRepository extends JpaRepository<Faculty, Integer> {
 
 }
