cf344740bec8ad604579607c254789113d2bc83a
taras2480 <aviator2480@gmail.com>
2020-04-06 15:10:19 +0300

    added annotation @Repository

--- admissions-committee/src/main/java/ua/lviv/lgs/admissionsCommittee/dao/UserRepositoriy.java
+++ admissions-committee/src/main/java/ua/lviv/lgs/admissionsCommittee/dao/UserRepositoriy.java
@@ -3,9 +3,11 @@ package ua.lviv.lgs.admissionsCommittee.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ua.lviv.lgs.admissionsCommittee.domain.User;
 
+@Repository
 public interface UserRepositoriy extends JpaRepository<User, Integer> {
 
 	List<User> findByEmail(String email);
