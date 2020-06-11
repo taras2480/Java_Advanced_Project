eda001f5cdf1531b6289d434d22e92c23992c5cd
taras2480 <aviator2480@gmail.com>
2020-06-12 01:00:59 +0300

    added Component Scan and some tests cod

--- src/main/java/ua/lviv/lgs/admissionsCommittee/AdmissionsсommitteeApplication.java
+++ src/main/java/ua/lviv/lgs/admissionsCommittee/AdmissionsсommitteeApplication.java
@@ -2,8 +2,10 @@ package ua.lviv.lgs.admissionsCommittee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"ua.lviv.lgs.admissionsCommittee"})
 public class AdmissionsсommitteeApplication {
 
 	public static void main(String[] args) {
